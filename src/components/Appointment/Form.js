d8c5c810cdb806d2492ba4617315029a0361c9e4
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-10-27 20:53:01 -0600

    add all components and test with storybook

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Button from "components/Button.js";
+import InterviewerList from "components/InterviewerList";
+
+export default function Form(props) {
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [name, setName] = useState(props.name || "");
+
+  function reset() {
+    setName("");
+    setInterviewer(null);
+  }
+
+  function cancel() {
+    reset();
+    props.onCancel();
+  }
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            type="text"
+            placeholder="Enter Student Name"
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
+}
