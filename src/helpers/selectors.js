9d6f524fb49dcd706e0d45388c9e8bb0e601a5d7
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-10-28 18:19:17 -0600

    add helper function getAppointmentForDay and run through tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,12 @@
 export function getAppointmentsForDay(state, day) {
-  const filteredDays = state.days.filter((day) => {
-    return day.name === day;
-  });
+  const filteredDays = state.days.find((dayObj) => dayObj.name === day);
+
+  const apptArray = [];
+
+  if (state.appointments && filteredDays) {
+    filteredDays.appointments.forEach((appId) =>
+      apptArray.push(state.appointments[appId])
+    );
+  }
+  return apptArray;
 }
