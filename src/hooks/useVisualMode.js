ae708120efc509563c2a65ba7e62963098f2f392
Kourtney Huget <kourtneyhuget@hotmail.com>
2020-10-29 16:32:03 -0600

    add mode, transition, back functions

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+export default function useVisualMode(initial) {
+  const [state, setState] = useState([initial]);
+  function transition(mode, replace) {
+    const newState = [...state];
+    if (replace) {
+      newState.pop();
+    }
+    newState.push(mode);
+    setState(newState);
+  }
+
+  const back = function () {
+    if (state.length < 2) {
+      return;
+    }
+    const newState = [...state];
+    newState.pop();
+    setState(newState);
+  };
+
+  const mode = state.slice(-1)[0];
+  return { mode, transition, back };
+}
